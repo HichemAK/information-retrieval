e549fa77446e7ef5576f03fe7a3845ae4568215a
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-02-06 16:57:45 +0100

    Solving issues related to the TF-IDF weights

--- models/vector_space_model.py
+++ models/vector_space_model.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 from enum import Enum, auto
 
+
 class SimilarityFunctions(Enum):
     DOT = auto()
     DICE = auto()
@@ -16,13 +17,13 @@ class VectorSpaceModel:
     def __init__(self, dictionary):
         self._dict = TermDocumentDict(dictionary)
         self._dict.weight_inplace()
-        self._norm_documents = {doc : np.linalg.norm(list(self._dict.terms(doc).values())) for doc in
+        self._norm_documents = {doc: np.linalg.norm(list(self._dict.terms(doc).values())) for doc in
                                 self._dict.all_documents}
         self._sim = {
-            SimilarityFunctions.DOT : self._eval_dot_product,
-            SimilarityFunctions.COSINUS : self._eval_cosinus,
-            SimilarityFunctions.DICE : self._eval_dice,
-            SimilarityFunctions.JACCARD : self._eval_jaccard
+            SimilarityFunctions.DOT: self._eval_dot_product,
+            SimilarityFunctions.COSINUS: self._eval_cosinus,
+            SimilarityFunctions.DICE: self._eval_dice,
+            SimilarityFunctions.JACCARD: self._eval_jaccard
         }
 
     def _eval_dot_product(self, l):
@@ -37,19 +38,19 @@ class VectorSpaceModel:
 
     def _eval_dice(self, l):
         document_scores = self._eval_dot_product(l)
-        document_scores = {k: 2*v/(self._norm_documents[k] ** 2 + len(l)) for k, v in document_scores.items()}
+        document_scores = {k: 2 * v / (self._norm_documents[k] ** 2 + len(l)) for k, v in document_scores.items()}
         return document_scores
 
     def _eval_cosinus(self, l):
         document_scores = self._eval_dot_product(l)
-        document_scores = {k: v/(self._norm_documents[k] * math.sqrt(len(l))) for k, v in document_scores.items()}
+        document_scores = {k: v / (self._norm_documents[k] * math.sqrt(len(l))) for k, v in document_scores.items()}
         return document_scores
 
     def _eval_jaccard(self, l):
         document_scores = self._eval_dot_product(l)
-        document_scores = {k : v/(self._norm_documents[k] ** 2 + len(l) - v) for k, v in document_scores.items()}
+        document_scores = {k: v / (self._norm_documents[k] ** 2 + len(l) - v) for k, v in document_scores.items()}
         return document_scores
 
     def eval(self, query, similarity_function=SimilarityFunctions.DOT):
-        l = re.findall(r'\w+', query)
-        return self._sim[similarity_function](l)
+        query_terms = re.findall(r'\w+', query)
+        return self._sim[similarity_function](query_terms)
